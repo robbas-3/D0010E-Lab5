d2ae5d166d4b3648237f181246d7499a1aeaa37d
robbas-3 <robin.bassili@hotmail.com>
2015-03-08 15:44:30 +0100

    StartEvent.java

---
+++ StartEvent.java
@@ -0,0 +1,25 @@
+package evvent;
+
+import lists.EventQueue;
+import states.SimState;
+/**
+ * StartEvent is an Event that starts the simulation.
+ * @author Robin
+ *
+ */
+public class StartEvent extends Event {
+	/**
+	 * Skuggning
+	 * @param state sending state so it can be changed accordingly.
+	 * @param eventQueue making it occur and jumps to next one
+	 */
+	public StartEvent(double time){
+		super(time);
+	}
+	public void execEvent(SimState state,EventQueue eventQueue){
+		// hmm gör något kul här
+		state.start();
+		eventQueue.next(); // hoppar till nästa??
+		
+	}
+}
