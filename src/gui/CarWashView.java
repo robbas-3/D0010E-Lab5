3e84656078b14619a643d076d6a0c65afa7ef6e8
chrjns-derp <christoffer.jonsson.grevie@gmail.com>
2015-03-10 20:34:57 +0100

    CWS
    la till lite av update

--- src/gui/CarWashView.java
+++ src/gui/CarWashView.java
@@ -19,8 +19,8 @@ public class CarWashView extends SimView {
 		 write("Fast machines: %d", CWState.fastCarWashes());
 		 write("Fast machines: %d", CWState.slowCarWashes());
 		 
-		 write("Fast distribution: (%.1f, %.1f)", CWState.getFastLower(), CWState.getFastUpper();
-	     write("Slow distribution: (%.1f, %.1f)", CWState.getSlowLower(), CWState.getSlowUpper()));
+		 write("Fast distribution: (%.1f, %.1f)", CWState.getFastLower(), CWState.getFastUpper());
+	     write("Slow distribution: (%.1f, %.1f)", CWState.getSlowLower(), CWState.getSlowUpper());
 	     write("Exponential distribution with lambda = %.1f", CWState.getLambda());
 		 
 		 write("Seed = %d", CWState.getSeed());
@@ -30,23 +30,25 @@ public class CarWashView extends SimView {
 		 
 		
 	}
-	private void write(String string, double fastLower, double fastUpper,
-			Object write) {
-		// TODO Auto-generated method stub
-		
-	}
-	private void write(String string, double fastLower, double fastUpper,
-			Object write) {
-		// TODO Auto-generated method stub
-		
-	}
 	private void Writelast(){
 		write("---------------------------");
 	    write("Total idle matchine time: %.2f", CWState.getIdleTime());
-	    write("Total queueing time: %.2f", CWState.getTime());//queue time total
+	    write("Total queueing time: %.2f", CWState.getTime());
 	    write("Mean queueing time: %.2f", CWState. ); //?
 	    write("Rejected cars: %d", CWState.getRejectedCars());
 	}
 	
+	public void update(Observable o, Object arg){
+		
+		super.update(o, arg);
+		
+		if ( instanceof ArriveEvent){
+			
+		}
+		
+		if ( instanceof LeavEvent){
+			
+		}
+	}
 	
 }
