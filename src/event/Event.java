9933c187d8c2d113f85ea00a2743c8615079c669
robbas-3 <robin.bassili@hotmail.com>
2015-03-10 20:30:26 +0100

    Update Event.java

--- src/event/Event.java
+++ src/event/Event.java
@@ -1,21 +1,23 @@
 package event;
 
+import model.SimState;
+
 //import states.SimState;
 
 /*
  * Skapar bland annat event som skickas till sorted Seq som sorterar den
  * denna sorterade sekvens sparas sedan i eventQueue
  */
-public class Event implements Comparable <Event> {
+public abstract class Event implements Comparable <Event> {
 	
 	//public SimState state;
 	private SortedSequence sortedSequence;
 	private String name;
 	public double eventTime;
 
-	public Event(double time,String s) {
-		eventTime = time;
-		name = s;
+	public Event(double time, String s) {
+		this.eventTime = time;
+		this.name = s;
 	}
 
 	public double getEventTime() {
@@ -29,7 +31,8 @@ public class Event implements Comparable <Event> {
 	 *            making it occur and jumps to next one
 	 * @return
 	 */
-	public void execEvent( EventQueue eventQueue) {
+	public void execEvent(SimState state,EventQueue eventQueue){
+		
 	}
 
 	// hmm gör något kul här
@@ -40,7 +43,10 @@ public class Event implements Comparable <Event> {
 	 *            event
 	 */
 	public void createNewEvent(SortedSequence sortedSequence, Event event) {
-		sortedSequence.addNsort(event);
+		this.sortedSequence.addNsort(event);
+	}
+	public void createNextEvent(double time){
+		//Event newEvent = new Event(time);
 	}
 
 	/**
@@ -63,3 +69,4 @@ public class Event implements Comparable <Event> {
 			return -1;
 		}
 	}
+}
