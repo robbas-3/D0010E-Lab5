c59a706a8a0e0e16ccc75dd4021f4e321427a56b
robbas-3 <robin.bassili@hotmail.com>
2015-03-10 22:47:42 +0100

    Update ArriveEvent.java

--- src/event/ArriveEvent.java
+++ src/event/ArriveEvent.java
@@ -14,42 +14,44 @@ public class ArriveEvent extends Event {
 	EventQueue eventQueue;
 	CarFactory carFactory;
 	
-	private double arriveTime;
+
 
 	public ArriveEvent(double time, String s,EventQueue eventQueue, CarWashState state) {
 		super(time,s);
-//		this.eventQueue = eventQueue;
-//		this.state = state;
-		
-		
-		
+		this.eventQueue = eventQueue;
+		this.state = state;	
 	}
-
 		/**
 		 * updates state and queue
 		 */
-	public void execEvent(SimState state,EventQueue eventQueue) {
-		//state changes
+	public void execEvent() {
 		
-//arriveTime +=state.getTime();
+		state.setEvent(this);
 		
-//		state.getIdleTime+= // ska utveckla denna
-		// queue changes
-	if(this.state.emptyFastCarWashes()!=0 || this.state.emptySlowCarWashes()!=0){
-//		
-			
-	
-		
-//		LeaveEvent newLeaveEvent = new LeaveEvent(arriveTime, "Leave");
-		createNewEvent(eventQueue.getSortedSequence(),new CarWashEvent(fastCWorSlowCW(),"",this.state.addCar(carFactory.createCar()))); // ändra till rätt tid.
-//		setChanged();
-//		notifyObservers();
+		if(this.state.emptyFastCarWashes()!=0 || this.state.emptySlowCarWashes()!=0){
+			createNextEvent(fastCWorSlowCW(),new CarWashEvent(fastCWorSlowCW(),"",this.state.addCar(carFactory.createCar())));
+		}
 	}
 	
-		// två metoder som ger random slow time och fast. gör ny car wash event då ska jag använda den metoden för att skicka med en tid.
+	@Override
+	public void createNextEvent(double time, Event event) {
+		createNewEvent(eventQueue.getSortedSequence(),event);
 	}
-// leave event där ska jag också göra en ny carwash event om kön är ledig. eller i simulatorn.
 	public double fastCWorSlowCW(){
 		return (this.state.addCar(carFactory.createCar())) == this.fCW ? this.state.getFastTime() : this.state.getSlowTime();
 	}
 }
+//arriveTime +=state.getTime();
+// två metoder som ger random slow time och fast. gör ny car wash event då ska jag använda den metoden för att skicka med en tid.
+
+//state.getIdleTime+= // ska utveckla denna
+// queue changes
+//,"",this.state.addCar(carFactory.createCar()))
+//leave event där ska jag också göra en ny carwash event om kön är ledig. eller i simulatorn.
+
+//this.state.setEvent(event);;
+
+//LeaveEvent newLeaveEvent = new LeaveEvent(arriveTime, "Leave");
+//createNewEvent(eventQueue.getSortedSequence(),new CarWashEvent(fastCWorSlowCW(),"",this.state.addCar(carFactory.createCar()))); // ändra till rätt tid.
+//setChanged();
+//notifyObservers();
