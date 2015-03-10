e89ac5a2ad3f9a30bdac6165ed29d408dbc49ab0
robbas-3 <robin.bassili@hotmail.com>
2015-03-10 10:22:41 +0100

    Update StopEvent.java

--- StopEvent.java
+++ StopEvent.java
@@ -1,27 +1,27 @@
-package evvent;
+package event;
 
-import lists.EventQueue;
-import states.SimState;
 /**
  * Stopevent is a generalEvent which stops the simulation.
+ * 
  * @author Robin
- *
+ * 
  */
 public class StopEvent extends Event {
-	
-	public StopEvent(double time){
-		super(time);
+	public StopEvent(double time) {
+		super(time,"Stop");
 	}
+
 	/**
 	 * 
-	 * @param state changing state to stop, halts the simulation.
-	 * @param eventQueue clearing eventQueue from all future events.
+	 * @param state
+	 *            changing state to stop, halts the simulation.
+	 * @param eventQueue
+	 *            clearing eventQueue from all future events.
 	 */
-	public void execEvent(SimState state,EventQueue eventQueue){
+	public void execEvent(SimState state, EventQueue eventQueue) {
 		// hmm gör något kul här
 		state.stop();
 		eventQueue.getSortedSequence().clearSeq();
-		 // behöver utvecklas mer??
-		
+		// behöver utvecklas mer??
 	}
 }
