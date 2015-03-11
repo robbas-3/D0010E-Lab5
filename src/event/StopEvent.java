1a62f9e93dc9d5308bd3e71e3ae55559d760f897
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 23:11:42 +0100

    JavaDoc

--- src/event/StopEvent.java
+++ src/event/StopEvent.java
@@ -4,39 +4,37 @@ import model.SimState;
 import simulator.Simulator;
 
 /**
- * Stopevent is a generalEvent which stops the simulation.
- * 
- * @author Robin
- * 
+ * Event that stops the simulation.
  */
 public class StopEvent extends Event {
 	private Simulator simulator;
-	public StopEvent(double time, String s,Simulator simulator) {
-		super(time,s);
+
+	public StopEvent(double time, String s, Simulator simulator) {
+		super(time, s);
 		this.simulator = simulator;
 	}
 
 	/**
+	 * execute the stop event.
 	 * 
 	 * @param state
 	 *            changing state to stop, halts the simulation.
 	 * @param eventQueue
 	 *            clearing eventQueue from all future events.
 	 */
-	
-	@Override
 	public void execEvent(SimState state, EventQueue eventQueue) {
-	
+
 		state.setEvent(this);
 		eventQueue.getSortedSequence().clearSeq();
 		state.end();
 		simulator.stop();
-		
+
 	}
 
-	@Override
-	public void createNextEvent(Event event,EventQueue eventQueue) {
-		
-		
+	/**
+	 * Do not create a future event.
+	 */
+	public void createNextEvent(Event event, EventQueue eventQueue) {
+
 	}
 }
