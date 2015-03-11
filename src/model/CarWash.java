892a2dd0839864d1def4de5b741ad6b69f8f7f9b
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 23:14:02 +0100

    JavaDoc

--- src/model/CarWash.java
+++ src/model/CarWash.java
@@ -1,28 +1,55 @@
 package model;
 
+/**
+ * 
+ * Class that represents the a car wash.
+ *
+ */
 public class CarWash {
-	
+
 	private Car car = null;
 	private boolean empty = true;
-	
-	public Car getCar(){
+
+	/**
+	 * Method for returning a car.
+	 * 
+	 * @return returns a car.
+	 */
+	public Car getCar() {
 		return car;
 	}
-	
-	public void cleanCar(){
+
+	/**
+	 * Method for cleaning a car, no longer needed in a car wash
+	 */
+	public void cleanCar() {
 		car = null;
 	}
-	
-	public void setCar(Car car){
+
+	/**
+	 * Putting a car in a car wash
+	 * 
+	 * @param car
+	 */
+	public void setCar(Car car) {
 		this.car = car;
 		empty = false;
 	}
-	
-	public boolean isEmpty(){
+
+	/**
+	 * Checking if car wash is empty
+	 * 
+	 * @return returning either true or false.
+	 */
+	public boolean isEmpty() {
 		return empty;
 	}
-	public void emptyCarWash(){
+
+	/**
+	 * The cleaned car are being removed from the car wash by this method.
+	 */
+	public void emptyCarWash() {
 		empty = true;
 	}
-	
+
 }
