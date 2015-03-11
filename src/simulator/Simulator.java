4bcb76c359688a297cc3b721bb0c3fc7d63c314a
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 23:15:47 +0100

    JavaDoc

--- src/simulator/Simulator.java
+++ src/simulator/Simulator.java
@@ -8,16 +8,24 @@ import event.StopEvent;
 import gui.CarWashView;
 import model.CarWashState;
 
+/**
+ * 
+ * Class that simulates.
+ *
+ */
 public class Simulator {
 
 	private boolean on;
-	
-	public Simulator(){
+
+	/**
+	 * Constructor using a driver.
+	 */
+	public Simulator() {
 		on = true;
 		Driver();
 	}
-	
-	private void Driver(){
+
+	private void Driver() {
 		int seed = 1234;
 		int fastAmount = 2;
 		int slowAmount = 2;
@@ -27,38 +35,54 @@ public class Simulator {
 		double slowLower = 3.5;
 		double slowUpper = 6.7;
 		double lambda = 2.0;
-		
-		CarWashState cws = new CarWashState(seed, fastAmount, slowAmount, queueSize, fastLower, fastUpper, slowLower, slowUpper, lambda);
+
+		CarWashState cws = new CarWashState(seed, fastAmount, slowAmount,
+				queueSize, fastLower, fastUpper, slowLower, slowUpper, lambda);
 		CarWashView cwv = new CarWashView(cws);
 		EventQueue eq = new EventQueue(new SortedSequence());
-		eq.getSortedSequence().addNsort(new StartEvent(0,"Start", this));;
-		eq.getSortedSequence().addNsort(new StopEvent(15,"Stop", this));;
-		
+		eq.getSortedSequence().addNsort(new StartEvent(0, "Start", this));
+		;
+		eq.getSortedSequence().addNsort(new StopEvent(15, "Stop", this));
+		;
+
 		double time = 0;// Test
-		while(time < 15.0){
+		while (time < 15.0) {
 			time += cws.arrivalTime();
-			eq.getSortedSequence().addNsort(new ArriveEvent(time, "Arrive", eq, cws));		
+			eq.getSortedSequence().addNsort(
+					new ArriveEvent(time, "Arrive", eq, cws));
 		}
-		while(on){
-			if(eq.hasNext()){
-				if(eq.getSortedSequence().getElement(0).getEventTime() < cws.getTime()){
-					eq.next().execEvent(cws,eq);
+		while (on) {
+			if (eq.hasNext()) {
+				if (eq.getSortedSequence().getElement(0).getEventTime() < cws
+						.getTime()) {
+					eq.next().execEvent(cws, eq);
 				}
 			}
 		}
-		
+
 	}
-	public void start(){
+
+	/**
+	 * changing the on variable
+	 */
+	public void start() {
 		on = true;
 	}
-	public void stop(){
+
+	/**
+	 * changing the on variable
+	 */
+	public void stop() {
 		on = false;
 	}
-	
-	public static void main(String[] args){
+
+	/**
+	 * creates a simulator
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
 		Simulator simulator = new Simulator();
 	}
 
-	
 }
-
