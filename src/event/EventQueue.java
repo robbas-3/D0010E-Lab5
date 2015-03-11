0520acfba999841d2bc575fd83b5ed61da83643a
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 23:10:14 +0100

    JavaDoc

--- src/event/EventQueue.java
+++ src/event/EventQueue.java
@@ -1,44 +1,57 @@
 package event;
 
+/**
+ * This class represents the queue of events
+ * 
+ * 
+ *
+ */
 public class EventQueue {
 	private SortedSequence sortSeq;
 
-	public EventQueue(SortedSequence sortSeq){
+	/**
+	 * The constructor
+	 * 
+	 * @param sortSeq
+	 *            a sequence that is sorted by events time values.
+	 */
+	public EventQueue(SortedSequence sortSeq) {
 		this.sortSeq = sortSeq;
 	}
 
 	/**
-	 * Checking if the queue has another element.
+	 * Checking if the queue has another element. if the sequence of events
+	 * equals 0 then we don't have any more events
 	 */
 	public Boolean hasNext() {
-		if (sortSeq.getSize()  == 0) {
-			//System.out.println("finns inga mer event.");
+		if (sortSeq.getSize() == 0) {
+
 			return false;
 		} else {
 			return true;
 		}
 	}
- 
+
 	/**
 	 * Taking the first element in the queue and then removes it from the
-	 * sortedSeq. Because if we execute an event it must PERISH!
+	 * sortedSequence. Because if we execute an event it disappears.
 	 */
 	public Event next() {
-		if (hasNext()){
+		if (hasNext()) {
 			Event nextElement = sortSeq.getElement(0);
 			sortSeq.removeEvent(0);
 			return nextElement;
-	
-		}
-		else {
+
+		} else {
 			return null;
 		}
-	
+
 	}
 
 	/**
+	 * Method to return the sorted sequence.
 	 * 
-	 * @return returns the private var sortSeq.
+	 * @return returns the private variable sortSeq.
 	 */
 	public SortedSequence getSortedSequence() {
 		return sortSeq;
