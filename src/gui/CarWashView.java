2c0354264397edb99d2ffdbf88dcd241c9d91603
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 23:12:23 +0100

    JavaDoc

--- src/gui/CarWashView.java
+++ src/gui/CarWashView.java
@@ -11,78 +11,93 @@ import model.Car;
 import model.CarWashState;
 import model.SimState;
 
+/**
+ * 
+ * Class for showing the view of a simulation of a car wash
+ *
+ */
 public class CarWashView extends SimView {
 
 	private CarWashState CWState;
-	
-	public CarWashView(SimState state){
+
+	/**
+	 * Constructor
+	 * 
+	 * @param state
+	 *            state.
+	 */
+	public CarWashView(SimState state) {
 		super(state);
-		
-		CWState = (CarWashState)state;
+
+		CWState = (CarWashState) state;
 		Writefirst();
 	}
-	private void Writefirst(){
-		 write("Fast machines: %d", CWState.fastCarWashes());
-		 write("Fast machines: %d", CWState.slowCarWashes());
-		 
-		 write("Fast distribution: (%.1f, %.1f)", CWState.getFastLower(), CWState.getFastUpper());
-	     write("Slow distribution: (%.1f, %.1f)", CWState.getSlowLower(), CWState.getSlowUpper());
-	     write("Exponential distribution with lambda = %.1f", CWState.getLambda());
-		 
-		 write("Seed = %d", CWState.getSeed());
-	     write("Max Queue size: %d", CWState.getQueueSize());
-	     write("-----------------------------------");
-	     write("%-8s%-6s%-6s%-5s%-11s%-10s%-11s%-11s%-10s", "Time", "Fast", "Slow", "Id", "Event", "IdleTime", "QueueTime", "QueueSize", "Rejected");
-	     write("%-8.2f%-6d%-6d%-5s%-11s%-10.2f%-11.2f%-11d%-10d",
-	                0.0,
-	                CWState.emptyFastCarWashes(),
-	                CWState.emptySlowCarWashes(),
-	                "-",
-	                "Start",
-	                CWState.getIdleTime(),
-	                CWState.getQueueTime(),
-	                CWState.getCarQueueSize(),
-	                CWState.getRejectedCars());
-		
+
+	/**
+	 * Method for the writing that should be displayed at the beginning.
+	 */
+	private void Writefirst() {
+		write("Fast machines: %d", CWState.fastCarWashes());
+		write("Fast machines: %d", CWState.slowCarWashes());
+
+		write("Fast distribution: (%.1f, %.1f)", CWState.getFastLower(),
+				CWState.getFastUpper());
+		write("Slow distribution: (%.1f, %.1f)", CWState.getSlowLower(),
+				CWState.getSlowUpper());
+		write("Exponential distribution with lambda = %.1f",
+				CWState.getLambda());
+
+		write("Seed = %d", CWState.getSeed());
+		write("Max Queue size: %d", CWState.getQueueSize());
+		write("-----------------------------------");
+		write("%-8s%-6s%-6s%-5s%-11s%-10s%-11s%-11s%-10s", "Time", "Fast",
+				"Slow", "Id", "Event", "IdleTime", "QueueTime", "QueueSize",
+				"Rejected");
+		write("%-8.2f%-6d%-6d%-5s%-11s%-10.2f%-11.2f%-11d%-10d", 0.0,
+				CWState.emptyFastCarWashes(), CWState.emptySlowCarWashes(),
+				"-", "Start", CWState.getIdleTime(), CWState.getQueueTime(),
+				CWState.getCarQueueSize(), CWState.getRejectedCars());
+
 	}
-	public void Writelast(){
+
+	/**
+	 * Method for writing that should be displayed at the end.
+	 */
+	public void Writelast() {
 		write("---------------------------");
-	    write("Total idle matchine time: %.2f", CWState.getIdleTime());
-	    write("Total queueing time: %.2f", CWState.getQueueTime());
-	    write("Mean queueing time: %.2f", CWState.meanQueueingTime() ); 
-	    write("Rejected cars: %d", CWState.getRejectedCars());
+		write("Total idle matchine time: %.2f", CWState.getIdleTime());
+		write("Total queueing time: %.2f", CWState.getQueueTime());
+		write("Mean queueing time: %.2f", CWState.meanQueueingTime());
+		write("Rejected cars: %d", CWState.getRejectedCars());
 	}
-	
-	public void update(Observable o, Object arg){
-		
-		
+
+	/**
+	 * Method that updates the console view with the current data.
+	 */
+	public void update(Observable o, Object arg) {
+
 		super.update(o, arg);
 		Event ev = CWState.getCurrentEvent();
-		Car c = (Car)arg;
-		
+		Car c = (Car) arg;
+
 		String id = "-";
-		if (ev instanceof ArriveEvent){
-			id = Integer.toString(c.getId()); 
+		if (ev instanceof ArriveEvent) {
+			id = Integer.toString(c.getId());
 		}
-		
-		if (ev instanceof LeaveEvent){
+
+		if (ev instanceof LeaveEvent) {
 			id = Integer.toString(c.getId());
 		}
 		write("%-8.2f%-6d%-6d%-5s%-11s%-10.2f%-11.2f%-11d%-10d",
-                ev.getEventTime(),
-                CWState.emptyFastCarWashes(),
-                CWState.emptySlowCarWashes(),
-                id,
-                ev.getName(),
-                CWState.getIdleTime(),
-                CWState.getQueueTime(),
-                CWState.getCarQueueSize(),
-                CWState.getRejectedCars());
-		
-		if(ev instanceof StopEvent) {
+				ev.getEventTime(), CWState.emptyFastCarWashes(),
+				CWState.emptySlowCarWashes(), id, ev.getName(),
+				CWState.getIdleTime(), CWState.getQueueTime(),
+				CWState.getCarQueueSize(), CWState.getRejectedCars());
+
+		if (ev instanceof StopEvent) {
 			Writelast();
 		}
-		
+
 	}
-	
+
 }
