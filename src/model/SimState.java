3aef509d552146a96fd09be67e4b30f0abcfc091
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 23:14:54 +0100

    JavaDoc

--- src/model/SimState.java
+++ src/model/SimState.java
@@ -5,30 +5,57 @@ import java.util.Observable;
 
 import event.Event;
 
+/**
+ * 
+ * Abstract class simstate that extends observable
+ *
+ */
 public abstract class SimState extends Observable {
-	
+
 	private double time;
 	private Event currentEvent;
-	
-	public SimState(){
+
+	/**
+	 * constructor sets the time
+	 */
+	public SimState() {
 		time = System.currentTimeMillis();
 	}
-	
-	public double getTime(){
-		return (System.currentTimeMillis() - time)/1000;
+
+	/**
+	 * gets the current time
+	 * 
+	 * @return value of the current time
+	 */
+	public double getTime() {
+		return (System.currentTimeMillis() - time) / 1000;
 	}
-	
-	public Event getCurrentEvent(){
+
+	/**
+	 * getting the current event that we are in
+	 * 
+	 * @return returns that event
+	 */
+	public Event getCurrentEvent() {
 		return currentEvent;
 	}
-	
-	public void end(){
+
+	/**
+	 * an method for updates
+	 */
+	public void end() {
 		setChanged();
 		notifyObservers();
 	}
-	
-	public void setEvent(Event event){
+
+	/**
+	 * sets event to desired event
+	 * 
+	 * @param event
+	 *            the event that is set.
+	 */
+	public void setEvent(Event event) {
 		this.currentEvent = event;
 	}
-	
+
 }
