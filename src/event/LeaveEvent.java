39600f2d9081224e31c64e9549b9e9e0e9382fd6
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 11:24:52 +0100

    Update LeaveEvent.java

--- src/event/LeaveEvent.java
+++ src/event/LeaveEvent.java
@@ -1,26 +1,41 @@
 package event;
 
+import model.CarWashState;
 import model.SimState;
 
 public class LeaveEvent extends Event {
-
+CarWashState state;
 	public LeaveEvent( double time,String s) {
 		super(time,s);
-		// TODO Auto-generated constructor stub
-	}
-
-	
-	public void execEvent(SimState state, EventQueue eventQueue) {
-		// TODO Auto-generated method stub
 		
 	}
 
-
-
-	public void createNextEvent(double time, Event event) {
+	
+	public void execEvent(CarWashState state, EventQueue eventQueue) {
 		// TODO Auto-generated method stub
+		state.setEvent(this);
+		if(state.getCarQueueSize()==0){
+			// queue empty carwash machine +1
+		}
+		else{
+			// ta första bilen i kön (stått i kö längst) state.carQueue.SHABLAM.
+			// carFromQueue.carWashEvent-->LeaveEvent
+			if(state.emptyFastCarWashes()!=0){												// måste få bil från kön
+				createNextEvent(state.getFastTime(),new CarWashEvent(state.getFastTime(),"",state.addCar((state.getCarQueue().get(state.getCarQueueSize()-1))));
+			}
+			else if(state.emptySlowCarWashes()!=0){
+				createNextEvent(state.getSlowTime(),new CarWashEvent(state.getSlowTime(),"",state.addCar(car)),eventQueue);
+			}
+			
+		}
+	}
+
+
+	@Override
+	public void createNextEvent(double time, Event event,EventQueue eventQueue) {
+		createNewEvent(eventQueue.getSortedSequence(),event);
+		
 		
 	}
 
 }
-// kollar först om det finns bilar i queue.
