47a4bd2f0715f0d3dbdf99e9a65f4d41546fd981
robbas-3 <robin.bassili@hotmail.com>
2015-03-11 23:12:57 +0100

    JavaDoc

--- src/gui/SimView.java
+++ src/gui/SimView.java
@@ -6,22 +6,51 @@ import java.util.Observer;
 import event.Event;
 import model.SimState;
 
-public abstract class SimView implements Observer{
-	
+/**
+ * 
+ * abstract class that represents the view.
+ *
+ */
+public abstract class SimView implements Observer {
+
 	protected SimState state;
-	
+
+	/**
+	 * The constructor.
+	 * 
+	 * @param state
+	 */
 	public SimView(SimState state) {
-        this.state = state;
-        state.addObserver(this);
+		this.state = state;
+		state.addObserver(this);
 	}
+
+	/**
+	 * Not defined update method.
+	 */
 	public void update(Observable o, Object arg) {
-	        //Event e = (Event)arg;
+
 	}
-	    
+
+	/**
+	 * Method for displaying the object.
+	 * 
+	 * @param o
+	 *            object.
+	 */
 	protected void write(Object o) {
 		System.out.println(o);
-		}
+	}
+
+	/**
+	 * Method for using a formatted string.
+	 * 
+	 * @param str
+	 *            string
+	 * @param args
+	 *            objects
+	 */
 	protected void write(String str, Object... args) {
-		System.out.println(String.format(str, args));		
+		System.out.println(String.format(str, args));
 	}
 }
